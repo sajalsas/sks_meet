8b26bc366072e1864f8772cd32894b5778022172
unknown <sajalsas@gmail.com>
2024-06-26 16:55:17 +0530

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react';
+import { Metadata } from 'next';
+import StreamVideoProvider from '@/providers/StreamClientProvider';
+export const metadata: Metadata = {
+  title: "SKSMEET",
+  description: "Video calling app",
+  icons:{
+    icon:'/icons/logo.svg'
+  }
+};
+
+const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
+};
+
+export default RootLayout;
