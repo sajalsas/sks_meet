91391c4720e420e7cae1e23d3a29eeddae1c40f7
sajalsas <118689240+sajalsas@users.noreply.github.com>
2024-08-26 12:52:11 +0530

    Update MobileNav.tsx

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -34,7 +34,7 @@ const MobileNav = () => {
             alt="Yoom logo"
             className='max-sm:size-10'
             />
-            <p className="text-[26px] font-extrabold text-white max ">SKSMEET</p>
+            <p className="text-[26px] font-extrabold text-white max ">Hazel</p>
             </Link>
             <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
                 <SheetClose asChild>
